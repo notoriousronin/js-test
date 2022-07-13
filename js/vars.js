220cad69505ca8058a1b4bc6ad50f6e4a256a6d3
Andrey Gavrilyuk <andrew250519@gmail.com>
2022-07-14 00:02:48 +0300

    Testing js

---
+++ js/vars.js
@@ -0,0 +1,15 @@
+const age = 10;
+const totalPrice = 207.4;
+const userName = 'Bravo';
+const message = 'Welcome!';
+const isOpen = true;
+const shouldConfirm = false;
+
+// let quantity = prompt('Choose the value');
+// quantity = Number(quantity);
+// console.log(quantity);
+// console.log(typeof quantity);
+
+let elementWidth = '50px';
+const result = Number.parseInt(elementWidth);
+console.log(result);
