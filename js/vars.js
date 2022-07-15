1c7a8204460b52d8e1f08d6083a82a537f661ead
Andrey Gavrilyuk <andrew250519@gmail.com>
2022-07-16 00:22:26 +0300

    test

--- js/vars.js
+++ js/vars.js
@@ -1,15 +1,67 @@
-const age = 10;
-const totalPrice = 207.4;
-const userName = 'Bravo';
-const message = 'Welcome!';
-const isOpen = true;
-const shouldConfirm = false;
+// const age = 10;
+// const totalPrice = 207.4;
+// const userName = 'Bravo';
+// const message = 'Welcome!';
+// const isOpen = true;
+// const shouldConfirm = false;
 
 // let quantity = prompt('Choose the value');
 // quantity = Number(quantity);
 // console.log(quantity);
 // console.log(typeof quantity);
 
-let elementWidth = '50px';
-const result = Number.parseInt(elementWidth);
-console.log(result);
+// let elementWidth = '50.16px';
+// const result = Number.parseFloat(elementWidth);
+// console.log(result);
+
+// let salary = 78615.16257;
+// console.log(Number(salary.toFixed(2)));
+
+// const base = 5;
+// const power = 2;
+
+// const result = Math.pow(5, 0.5);
+
+// console.log(result);
+
+// let clientCounter = 1;
+// const maxClients = 25;
+
+// while (clientCounter < maxClients) {
+//   console.log(clientCounter);
+//   clientCounter += 2;
+// }
+
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+// console.log(message);
+
+// function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+//   const totalPrice = orderedQuantity * pricePerDroid + deliveryFee;
+//   const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+
+//   return message;
+// }
+
+// function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
+//   let message;
+//   // Change code below this line
+//   const totalPrice = pricePerDroid * orderedQuantity;
+//   if (totalPrice > customerCredits) {
+//     message = 'Insufficient funds!';
+//   } else if ((customerCredits = customerCredits - totalPrice)) {
+//     message = `You ordered ${orderedQuantity} droids, you have ${customerCredits} credits left`;
+//   } else {
+//     console.log('Invalid subscription type');
+//   }
+
+//   // Change code above this line
+//   return message;
+// }
+
+for (let i = 50; i >= 0; i -= 5) {
+  console.log(i);
+}
